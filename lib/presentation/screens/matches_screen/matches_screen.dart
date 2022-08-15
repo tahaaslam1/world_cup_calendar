32e2968a00d975051043e88de794a17b2789130d
Taha Aslam <k190147@nu.edu.pk>
2022-08-16 03:40:30 +0500

    update main

--- lib/presentation/screens/matches_screen/matches_screen.dart
+++ lib/presentation/screens/matches_screen/matches_screen.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class MatcheScreen extends StatelessWidget {
   const MatcheScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: Color(),
-    );
+    return const Scaffold();
   }
 }
