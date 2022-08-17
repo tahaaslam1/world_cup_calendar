fdde7d2d0c5be0888f579146c76c6dc104cee0c9
Taha Aslam <k190147@nu.edu.pk>
2022-08-18 04:09:26 +0500

    fixed everything

--- lib/presentation/screens/matches_screen/matches_screen.dart
+++ lib/presentation/screens/matches_screen/matches_screen.dart
@@ -2,15 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:world_cup_calendar/presentation/screens/matches_screen/local_widgets/matchcard.dart';
+import 'package:world_cup_calendar/presentation/screens/matches_screen/local_widgets/match_card.dart';
 
 class MatchScreen extends StatelessWidget {
-  var homeTeamFlag = 'assets/countries/senegal.png';
-  var awayTeamFlag = 'assets/countries/netherland.png';
-  var homeTeam = 'Senegal';
-  var awayTeam = 'Netherland';
-  var matchTime = '13:00';
-  var matchDate = '21 November 2022';
+  const MatchScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,36 +17,105 @@ class MatchScreen extends StatelessWidget {
           child: AppBar(
             centerTitle: true,
             backgroundColor: const Color(0xFF0C3C72),
-            flexibleSpace: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    onPressed: () => {},
-                    icon: const FaIcon(FontAwesomeIcons.caretLeft),
-                    color: Colors.white,
+            flexibleSpace: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () => {},
+                  icon: const FaIcon(FontAwesomeIcons.caretLeft),
+                  color: Colors.white,
+                ),
+                const Image(image: AssetImage("assets/player/messi.png")),
+                Text(
+                  'Matches',
+                  style: GoogleFonts.inter(
+                    textStyle:
+                        const TextStyle(fontSize: 37, color: Colors.white),
                   ),
-                  const Image(image: AssetImage("assets/player/messi.png")),
-                  Text(
-                    'Matches',
-                    style: GoogleFonts.inter(
-                      textStyle:
-                          const TextStyle(fontSize: 37, color: Colors.white),
-                    ),
-                  ),
-                  const Image(image: AssetImage("assets/player/chelsea.png"))
-                ],
-              ),
+                ),
+                const Image(image: AssetImage("assets/player/chelsea.png"))
+              ],
             ),
           ),
         ),
-        body: MatchCard(
-            homeTeamFlag: homeTeamFlag,
-            awayTeamFlag: awayTeamFlag,
-            homeTeam: homeTeam,
-            awayTeam: awayTeam,
-            matchTime: matchTime,
-            matchDate: matchDate),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color(0xFF000000).withOpacity(0.25),
+                      blurRadius: 4,
+                      spreadRadius: 1,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
+                ),
+                height: 48,
+                child: const Center(
+                  child: Text(
+                    '21 November 2022',    
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Color(0xFFFD4600),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 28,
+              ),
+              ListView(
+                shrinkWrap: true,
+                children: const [
+                  MatchCard(
+                    homeTeamFlag: 'assets/countries/senegal.png',
+                    awayTeamFlag: 'assets/countries/netherland.png',
+                    homeTeam: 'Senegal',
+                    awayTeam: 'Netherlands',
+                    matchTime: '13:00',
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  MatchCard(
+                    homeTeamFlag: 'assets/countries/senegal.png',
+                    awayTeamFlag: 'assets/countries/netherland.png',
+                    homeTeam: 'Senegal',
+                    awayTeam: 'Netherlands',
+                    matchTime: '13:00',
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  MatchCard(
+                    homeTeamFlag: 'assets/countries/senegal.png',
+                    awayTeamFlag: 'assets/countries/netherland.png',
+                    homeTeam: 'Senegal',
+                    awayTeam: 'Netherlands',
+                    matchTime: '13:00',
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  MatchCard(
+                    homeTeamFlag: 'assets/countries/senegal.png',
+                    awayTeamFlag: 'assets/countries/netherland.png',
+                    homeTeam: 'Senegal',
+                    awayTeam: 'Netherlands',
+                    matchTime: '13:00',
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
